1c873286a3c803babde62a14e1722f82b52d9843
Miguel Mesquita <mm@cob.pt>
2021-04-16 17:58:14 +0100

    feat: add autocomplete

--- bin/cob-cli.js
+++ bin/cob-cli.js
@@ -1,5 +1,33 @@
 #!/usr/bin/env node
 
+const path = require('path');
+const directoryPath = path.join(__dirname,"../lib/customizations/");
+
+const files = () => require('fs').readdirSync(directoryPath).filter(f => !f.startsWith("common")).map(f => f.substr(0,f.lastIndexOf(".")))
+
+/* Autocomplete Component*/
+var omelette = require('omelette');
+const commandStructure = {
+    'init' : [], 
+    'customize' : files, 
+    'test' : [], 
+    'deploy' : [], 
+    'updateFromServer' : []
+}
+const completion = omelette('cob-cli').tree(commandStructure)
+completion.init()
+
+// add to system profiles
+if (~process.argv.indexOf('--setup')) {
+    completion.setupShellInitFile()
+}
+// remove from system profiles
+if (~process.argv.indexOf('--cleanup')) {
+    completion.cleanupShellInitFile()
+}
+
+/*******************************************/
+
 const program = require('commander');
 
 const init             = require("../lib/commands/init");

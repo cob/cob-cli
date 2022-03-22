703914e405899031d1ab771b2f18e851a6cba11e
Miguel Mesquita <mm@cob.pt>
2022-03-22 11:48:25 +0000

    fix(customize): fill update info if field previously empty

--- templates/keywords/audit/frontend/js/cob/_audit.js
+++ templates/keywords/audit/frontend/js/cob/_audit.js
@@ -5,7 +5,7 @@ cob.custom.customize.push(function (core, utils, ui) {
         let userFPs = presenter.findFieldPs( fp => /[$]audit\.(creator|updater)\.(username|uri|time)/.exec(fp.field.fieldDefinition.description) )
         userFPs.forEach( fp => {
             fp.disable()
-            if(!instance.isNew() || presenter.isGroupEdit()) return //Only update if it's on create interface (updates will only be changed by the backend)
+            if(fp.getValue() && (!instance.isNew() || presenter.isGroupEdit())) return //Don't do nothing if field is not empty AND it's an update interface (updates will only be changed by the backend)
             if(/[$]audit\.(creator|updater)\.username/.exec(fp.field.fieldDefinition.description)) {
                 fp.setValue(core.getCurrentLoggedInUser())
             }

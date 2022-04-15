71ee7f075652b628dae084fd093a91ce9a1e130e
Miguel Mesquita <miguel.a.mesquita@gmail.com>
2022-04-15 17:40:32 +0100

    fix(customize): correctly copies fonts

--- templates/dashboards/dash/src/components/Menu.vue
+++ templates/dashboards/dash/src/components/Menu.vue
@@ -4,7 +4,7 @@
             :class="'transition ease-in-out duration-300 rounded-md border border-cobline border-l-2 border-l-sky-600 shadow-sm transform hover:translate-x-0.5 '
                     + item.text_style">
             <a :href="item.link" class="flex items-center font-light py-1 px-3">
-                <span class="">{{ item.text }}</span>
+                <span v-html="item.text"/>
             </a>
         </li>
     </ul>
--- templates/dashboards/dash/src/components/Title.vue
+++ templates/dashboards/dash/src/components/Title.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-slate-700 uppercase tracking-wider border-b border-b-cobline/50 pb-1 text-xs text-center">
-        {{ componentData.title_text }}
+        <span v-html="componentData.title_text"/>
     </div>
 </template>
 

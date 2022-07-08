6c48c1c59ea3e54a15bd4140c93461461fcd7871
jbarata <joao.barata@cultofbits.com>
2022-07-08 16:01:00 +0100

    feat(customize): dash - suporte para uso de nova opção Placeholder no filtro
    Para poder definir um texto alternativo ao default que aparece no campo quando vazio

--- templates/dashboards/dash/src/components/Filter.vue
+++ templates/dashboards/dash/src/components/Filter.vue
@@ -6,7 +6,7 @@
             @keyup.enter="applyFilter"
             @focus="resize"
             @keyup="resize"
-            placeholder="Pesquisar ..."
+            :placeholder="placeholder"
         ></textarea>
         <button @click="applyFilter" type="submit" class="max-h-11 p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
@@ -34,9 +34,10 @@
             }
         },
         computed: {
-            options()   { return this.component['FilterCustomize'][0] },
-            outputVar() { return this.component['OutputVarFilter'] || "" },
-            classes()   { return this.options['FilterClasses']     || "w-full max-w-xs resize-none min-h-min h-min border border-slate-300 rounded-md py-2 px-2 outline-slate-300 leading-5" },
+            options()     { return this.component['FilterCustomize'][0] },
+            outputVar()   { return this.component['OutputVarFilter'] || "" },
+            placeholder() { return this.options['Placeholder']       || "Pesquisar ..." },
+            classes()     { return this.options['FilterClasses']     || "w-full max-w-xs resize-none min-h-min h-min border border-slate-300 rounded-md py-2 px-2 outline-slate-300 leading-5" },
         },
         methods: {
             applyFilter: function() {

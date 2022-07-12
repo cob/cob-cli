1dda51f3e26aedb72586b8d7d50e0c9ce58a8129
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2022-07-12 19:06:52 +0100

    Execute query when we switch between months

--- templates/dashboards/dash/src/components/Calendar.vue
+++ templates/dashboards/dash/src/components/Calendar.vue
@@ -1,5 +1,12 @@
 <template>
-  <FullCalendar ref="fullCalendar" :options="calendarOptions"/>
+  <div class="relative">
+    <div class="absolute"></div>
+    <div>
+      <div class="mb-4 text-center text-4xl">{{ month }} {{ year }}</div>
+      <FullCalendar ref="fullCalendar" :options="calendarOptions"/>
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -19,45 +26,51 @@ export default {
 
   props: {component: Object},
 
-  data() {
-    return {
-      boardInfo: {},
+  data: () => ({
+    month: null,
+    year: null,
+    dateRange: null, // array: [initDate, endDate]
+    dashInfo: null, // Object, the result of new DashInfo(...)
 
-      calendarOptions: {
-        plugins: [dayGridPlugin, interactionPlugin, listPlugin],
-        initialView: 'dayGridMonth',
-        headerToolbar: {
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,listMonth'
-        },
-        buttonText: {
-          today: 'Today',
-          month: 'Month',
-          list: 'List',
-        },
-        height: "auto",
-        aspectRatio: 2,
-        validRange: {
-          start: '2017-01-01'
-        },
+    calendarOptions: {
+      plugins: [dayGridPlugin, interactionPlugin, listPlugin],
+      initialView: 'dayGridMonth',
+      headerToolbar: {
+        left: 'today prev next',
+        center: '',
+        right: 'dayGridMonth,listMonth'
+      },
+      buttonText: {
+        today: 'Today',
+        month: 'Month',
+        list: 'List',
+      },
+      height: "auto",
+      contentHeight: "auto",
+      aspectRatio: 2,
+      validRange: {
+        start: '2017-01-01'
       }
     }
-  },
+  }),
 
   computed: {
-    boardState() {
-      return this.boardInfo.results.state
-    },
-    boardResults() {
-      if (!this.boardInfo.results) return []
-      return this.boardInfo.results.value || []
+    dashResults() {
+      if (!this.dashInfo) return []
+      if (!this.dashInfo.results) return []
+      return this.dashInfo.results.value || []
     },
     options() {
       return this.component['CalendarCustomize'][0]
     },
+    query() {
+      if (!this.dateRange) return ""
+      const baseQuery = `${this.component['EventsQuery'] || "*"} AND ${this.component['DateEventField']}:[${this.dateRange[0].getTime()} TO ${this.dateRange[1].getTime()}]`
+      const inputVars = new Set(this.options['InputVarCalendar'].map(inputVar => inputVar['InputVarCalendar']));
+      return `${baseQuery}  ${[...inputVars].map(inputVar => this.component.vars[inputVar]).join(" ")}`.trim()
+    },
     events() {
-      return this.boardResults
+      return this.dashResults
           .filter(event => {
             const title = event[this.component['DescriptionEventField']] || [event.id]
             const date = event[this.component['DateEventField']]
@@ -85,13 +98,26 @@ export default {
   watch: {
     events: function(newEvents) {
       const calendarApi = this.$refs.fullCalendar.getApi()
-      calendarApi.getEvents().forEach(event => event.remove())
-      newEvents.forEach(event => calendarApi.addEvent(event))
+      calendarApi.batchRendering(() => {
+        calendarApi.getEvents().forEach(event => event.remove())
+        newEvents.forEach(event => calendarApi.addEvent(event))
+      })
+    },
+    query: function(newQuery) {
+      if (!newQuery) return
 
+      if (!this.dashInfo) {
+        console.debug("[dash][Calendar] New dashInfo created")
+        this.dashInfo = dashFunctions.instancesList(this.component['Definition'], this.query, 1000, 0, {validity: 30})
+
+      } else {
+        console.debug("[dash][Calendar] Updating dash info query")
+        this.dashInfo.changeArgs({query: newQuery})
+      }
     },
     'component.vars': {
       handler() {
-        this.loadEvents()
+        console.log("new input vars")
       },
       deep: true
     }
@@ -99,23 +125,38 @@ export default {
 
   mounted() {
     const calendarApi = this.$refs.fullCalendar.getApi()
-    calendarApi.gotoDate(new Date(2021, 10, 11))
+    calendarApi.setOption('datesSet', () => {this.updateDateRange()})
+    calendarApi.setOption('locale', this.getLocale())
 
     const dayMaxEvents = this.options['MaxVisibleDayEvents'] || MAX_VISIBLE_DAY_EVENTS
     calendarApi.setOption('dayMaxEvents', dayMaxEvents === -1 ? false : parseInt(dayMaxEvents, 10))
 
-    this.loadEvents()
+    this.updateDateRange()
+  },
+
+  beforeDestroy() {
+    if (this.dashInfo) {
+      this.dashInfo.stopUpdates()
+    }
   },
 
   methods: {
-    getQuery() {
-      const baseQuery = this.component['EventsQuery'] || ""
-      const inputVars = new Set(this.options['InputVarCalendar'].map(inputVar => inputVar['InputVarCalendar']));
-      const finalQuery = `${baseQuery} ${[...inputVars].map(inputVar => this.component.vars[inputVar]).join(" ")}`.trim()
-      return finalQuery ? finalQuery : "*"
+    updateDateRange() {
+      const calendarApi = this.$refs.fullCalendar.getApi()
+
+      calendarApi.batchRendering(() => {
+        calendarApi.getEvents().forEach(event => event.remove())
+      })
+
+      const currentDate = calendarApi.getDate()
+      this.month = currentDate.toLocaleString('default', { month: 'long' });
+      this.year = currentDate.getFullYear()
+
+      this.dateRange = [new Date(currentDate.getFullYear(), currentDate.getMonth(), 1), new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0)]
     },
-    loadEvents: function() {
-      this.boardInfo = dashFunctions.instancesList(this.component['Definition'], this.getQuery(), 255, 0, {validity: 30})
+    getLocale() {
+      if (navigator.languages !== undefined) return navigator.languages[0];
+      return navigator.language;
     },
     textToRGB: function(text) {
       let hash = 0;
@@ -141,3 +182,31 @@ export default {
   }
 }
 </script>
+
+<style lang='css'>
+
+.fc .fc-toolbar.fc-header-toolbar {
+  align-items: flex-end;
+  font-size: 0.8rem;
+}
+
+.fc .fc-toolbar.fc-header-toolbar .fc-toolbar-chunk:first-child button:not(:first-child) {
+  background-color: #fff;
+  border-color: #fff;
+  border-radius: 50%;
+  color: #000;
+  font-weight: 700;
+}
+
+.fc .fc-daygrid-day-top {
+  margin-bottom: 5px;
+  justify-content: center;
+  font-size: 0.8rem;
+}
+
+.fc .fc-daygrid-more-link {
+  top: 10px;
+  font-weight: 600;
+}
+
+</style>

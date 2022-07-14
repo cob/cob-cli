0538787dc1d172de875766f116b0fbb1d92fe916
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2022-07-14 11:10:16 +0100

    Simplify the code by creating some computed functions

--- templates/dashboards/dash/src/components/Calendar.vue
+++ templates/dashboards/dash/src/components/Calendar.vue
@@ -18,6 +18,7 @@ import listPlugin from '@fullcalendar/list'
 import * as dashFunctions from '@cob/dashboard-info';
 import Waiting from './shared/Waiting.vue'
 import debounce from 'lodash.debounce';
+import {toEsFieldName} from "@cob/rest-api-wrapper/src/utils/ESHelper";
 
 const DEFAULT_EVENT_COLOR = "#0e7bbe"
 const MAX_VISIBLE_DAY_EVENTS = 3
@@ -62,44 +63,59 @@ export default {
   }),
 
   computed: {
-    dashResults() {
-      if (!this.dashInfo) return []
-      if (!this.dashInfo.results) return []
-      return this.dashInfo.results.value || []
-    },
-    options() {
-      return this.component['CalendarCustomize'][0]
-    },
-    classes() {
-      return this.options['CalendarClasses'] || "p-4"
-    },
-    query() {
-      if (!this.dateRange) return ""
 
-      let dateRangeQuery = `${this.component['DateStartEventField']}:[${this.dateRange[0].getTime()} TO ${this.dateRange[1].getTime()}]`
-      if (this.component['DateEndEventField']) {
-        dateRangeQuery += ` OR ${this.component['DateEndEventField']}:[${this.dateRange[0].getTime()} TO ${this.dateRange[1].getTime()}]`
+    // Customizations component model
+    options() { return this.component['CalendarCustomize'][0] },
+    classes() { return this.options['CalendarClasses'] || "p-4" },
+    inputVarCalendar() { return this.options['InputVarCalendar'] || [] },
+    dayMaxEvents() {
+      try {
+        return parseInt(this.options['MaxVisibleDayEvents'], 10) || MAX_VISIBLE_DAY_EVENTS
+      } catch (e) {
+        return MAX_VISIBLE_DAY_EVENTS
+      }
+    },
+
+    // Calendar component model
+    definitionField() { return this.component['Definition'] },
+    descriptionEventField() { return toEsFieldName(this.component['DescriptionEventField']) },
+    startDateField() { return toEsFieldName(this.component['DateStartEventField']) },
+    endDateField() { return toEsFieldName(this.component['DateEndEventField']) },
+    stateField() { return toEsFieldName(this.component['StateEventField']) },
+    eventsQuery() { return this.component['EventsQuery'] || "*" },
+
+    query() {
+      if (!this.dateRange) return null
+
+      let dateRangeQuery = `${this.startDateField}:[${this.dateRange[0].getTime()} TO ${this.dateRange[1].getTime()}]`
+      if (this.endDateField) {
+        dateRangeQuery += ` OR ${this.endDateField}:[${this.dateRange[0].getTime()} TO ${this.dateRange[1].getTime()}]`
       }
 
-      const baseQuery = `${this.component['EventsQuery'] || "*"} AND (${dateRangeQuery})`
-      const inputVars = new Set(this.options['InputVarCalendar'].map(inputVar => inputVar['InputVarCalendar']));
-      const finalQuery = `${baseQuery}  ${[...inputVars].map(inputVar => this.component.vars[inputVar]).join(" ")}`.trim()
+      const baseQuery = `${this.eventsQuery} AND (${dateRangeQuery})`
+      const inputVars = new Set(this.inputVarCalendar.map(inputVar => inputVar['InputVarCalendar']));
+      const finalQuery = `${baseQuery} ${[...inputVars].map(inputVar => this.component.vars[inputVar]).join(" ")}`.trim()
 
       console.debug("[dash][Calendar] query:", finalQuery)
       return finalQuery
 
     },
+    dashResults() {
+      if (!this.dashInfo) return []
+      if (!this.dashInfo.results) return []
+      return this.dashInfo.results.value || []
+    },
     events() {
       return this.dashResults
           .filter(event => {
-            const title = event[this.component['DescriptionEventField']] || [event.id]
-            const date = event[this.component['DateStartEventField']]
+            const title = event[this.descriptionEventField] || [event.id]
+            const date = event[this.startDateField]
 
             return !!(title && date)
           })
           .map(event => {
-            const title = event[this.component['DescriptionEventField']] || [event.id]
-            const date = event[this.component['DateStartEventField']]
+            const title = event[this.descriptionEventField] || [event.id]
+            const date = event[this.startDateField]
 
             return {
               id: event.id,
@@ -107,9 +123,7 @@ export default {
               title: title[0] + (title.length > 1 ? `(${title.length})` : ""),
               start: new Date(parseInt(date[0])),
               allDay: true,
-              backgroundColor: this.component['StateEventField'] && event[this.component['StateEventField']]
-                               ? this.textToRGB(event[this.component['StateEventField']][0])
-                               : DEFAULT_EVENT_COLOR,
+              backgroundColor: this.stateField ? this.textToRGB(event[this.stateField][0]) : DEFAULT_EVENT_COLOR,
             }
           })
     },
@@ -118,6 +132,7 @@ export default {
   watch: {
     events: function(newEvents) {
       const calendarApi = this.$refs.fullCalendar.getApi()
+
       calendarApi.batchRendering(() => {
         calendarApi.getEvents().forEach(event => event.remove())
         newEvents.forEach(event => calendarApi.addEvent(event))
@@ -130,9 +145,11 @@ export default {
 
       if (!this.dashInfo) {
         console.debug("[dash][Calendar] New dashInfo created")
-        this.dashInfo = dashFunctions.instancesList(this.component['Definition'], this.query, 2000, 0, {validity: 30})
+        this.dashInfo = dashFunctions.instancesList(this.definitionField, this.query, 2000, 0, {validity: 30})
 
       } else {
+        this.showWaiting = true
+
         console.debug("[dash][Calendar] Updating dash info query")
         this.dashInfo.changeArgs({query: newQuery})
       }
@@ -161,8 +178,7 @@ export default {
       lazyEventsLoadHandler(dateInfo)
     })
 
-    const dayMaxEvents = this.options['MaxVisibleDayEvents'] || MAX_VISIBLE_DAY_EVENTS
-    calendarApi.setOption('dayMaxEvents', dayMaxEvents === -1 ? false : parseInt(dayMaxEvents, 10))
+    calendarApi.setOption('dayMaxEvents', this.dayMaxEvents === -1 ? false : this.dayMaxEvents)
     calendarApi.setOption('locale', this.getLocale())
   },
 
@@ -175,13 +191,6 @@ export default {
   methods: {
     updateDateRange(dateInfo) {
       this.showWaiting = true
-
-      const calendarApi = this.$refs.fullCalendar.getApi()
-
-      calendarApi.batchRendering(() => {
-        calendarApi.getEvents().forEach(event => event.remove())
-      })
-
       this.dateRange = [dateInfo.start, dateInfo.end]
     },
     getLocale() {
--- templates/dashboards/dash/src/components/shared/Waiting.vue
+++ templates/dashboards/dash/src/components/shared/Waiting.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-show="showWaiting" class="z-40 absolute inset-0 w-full h-full opacity-75">
-    <div class="absolute inset-0 bg-gray-400"></div>
+    <div class="absolute inset-0 bg-white"></div>
     <div class="flex justify-center items-center h-full">
       <i class="fas fa-spinner fa-spin fa-5x"></i>
     </div>

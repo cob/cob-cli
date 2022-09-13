4edb76a9e927704fd4a29a518b4d7f1be1c898e3
Miguel Mesquita <miguel.a.mesquita@gmail.com>
2022-09-13 16:11:32 +0100

    fix(customize.dash): corrects view link in totals

--- templates/dashboards/dash/src/components/TotalsValue.vue
+++ templates/dashboards/dash/src/components/TotalsValue.vue
@@ -43,7 +43,7 @@
             state()            { return this.valueData.dash_info && this.valueData.dash_info.state || "" },
             updating()         { return this.state === "updating" || this.state === "loading" },
             classes()          { return (this.options['ValueClasses'] || "Default Info") + " S_"+this.valueData.dash_info.state },
-            link()             { return this.valueData.dash_info.href + (this.view ? "?&av=" + this.view : "") },
+            link()             { return this.valueData.dash_info.href + (this.view ? "&av=" + this.view : "") },
             expandedClasses()  { return this.classes.split(/\s/).map(c => specialClasses[c] || c ).join(" ") },
             value() {
                 if(this.valueData.dash_info.state === "loading") return "L"

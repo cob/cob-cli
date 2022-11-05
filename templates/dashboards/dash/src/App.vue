c4ff604d8bca053bdf3799dc352c6bfea4f23fc9
Miguel Mesquita <miguel.a.mesquita@gmail.com>
2022-11-05 21:35:20 +0000

    refactor(customize.dsh): improve readability

--- templates/dashboards/dash/src/App.vue
+++ templates/dashboards/dash/src/App.vue
@@ -23,59 +23,43 @@ export default {
         name: 'App',
         components: { Dashboard },
         data: () => ({
-            userInfo: null,
-            name: null,
             error: "",
+            userInfo: null,
+            dashboardName: null,
             dashboardInstance: null,
-            dashboardInstanceId: Number,
             dashboardParsed: null,
             dashboardState: "Loading"
         }),
         created() {
             // At the initial load we get the dashboard instance name from the url
             umLoggedin().then( userInfo => {
-                let name = document.getElementsByClassName("custom-resource")[0].getAttribute('data-name').split(":")[0]
-                this.dashboardInstance = instancesList(DASHBOARD_DEF, this.getDashboardQuery(name, userInfo), 1)
+                this.userInfo = userInfo
+                this.dashboardName = document.getElementsByClassName("custom-resource")[0].getAttribute('data-name').split(":")[0]
+                this.dashboardInstance = instancesList(DASHBOARD_DEF, this.dashboardQuery, 1 /*just get the first*/)
             })
 
             // Upon anchor navigation we get the dashboard instance name from the first param to the 'resume' callback.
             $('section.custom-resource').on('resume', (e, params) => {
                 //Recheck user (the user might have changed or his groups might have changed after previous load)
                 umLoggedin().then(userInfo => {
-                  let name = params[0].split(":")[0]
-                  if( name !== this.name || this.userInfo.username !== userInfo.username ){
-                    this.dashboardInstance.changeArgs({query: this.getDashboardQuery(name, userInfo) })
-                  }
+                    let name = params[0].split(":")[0]
+                    if( name !== this.dashboardName || this.userInfo.username !== userInfo.username ){
+                        this.userInfo = userInfo
+                        this.dashboardName = name
+                        this.dashboardInstance.changeArgs({query: this.dashboardQuery })
+                    }
                 })
             });
         },
+        computed: {
+            dashboardQuery() {
+                let groups = this.userInfo.groups.map(g=> "\"" + g.name + "\"").join(" OR ")
+                let nameQuery = "name.raw:\"" + this.dashboardName + "\" "
+                let accessQuery = " (groupaccess.raw:(" + groups + ") OR (-groupaccess:*) )"
+                return "(" + nameQuery + accessQuery +") OR id:" + this.dashboardName
+            }
+        },
         watch: {
-            // Monitor changes to the id of the Dashboard instance
-             dashboardInstanceId(newInstanceId) {
-                axios
-                  .get("/recordm/recordm/instances/" + newInstanceId)
-                  .then(resp => {
-                      try {
-                          this.dashboardParsed = parseDashboard(resp.data, this.userInfo)
-                          this.dashboardState = "Ready"
-                      }
-                      catch(e) {
-                          this.error = "Error: error parsing dashboard " + newInstanceId + " (" + e + ")"
-                          this.dashboardState = "Error"
-                          console.error(e)
-                      }
-                  })
-                  .catch( (e) => {
-                      if( e.response && e.response.status && e.response.status === 403) {
-                          this.error = "New authorization required..."
-                      } else {
-                          this.error = "Error: error getting dashboard " + newInstanceId
-                      }
-                      this.dashboardState = "Error"
-                      console.error(e)
-                  })
-            },
-
             // Monitor state changes to the searching of the Dashboard instance
             'dashboardInstance.state'(instanceInfoState) {
                 if(instanceInfoState === "loading" || instanceInfoState === "updating") {
@@ -103,29 +87,37 @@ export default {
             },
 
             // Monitor value changes to the values of the Dashboard instance
-            'dashboardInstance.value'(newDashboards) {
-                if(newDashboards.length === 0) {
+            'dashboardInstance.value'(newDashboard) {
+                if(newDashboard.length === 0) {
                     this.dashboardState = "Error"
-                    this.error = "Error: dashboard '" + this.name + "' was not found for your user"
+                    this.error = "Error: dashboard '" + this.dashboardName + "' was not found for your user"
                 } else {
-                    //Instance(s) found (from ES). Use the 1st result.
-                    this.dashboardInstanceId =  newDashboards[0].id
+                    //Instance found (from ES) (we only asked for the 1st match)
+                    let newInstanceId =  newDashboard[0].id
+                    axios.get("/recordm/recordm/instances/" + newInstanceId)
+                    .then(resp => {
+                        try {
+                            this.dashboardParsed = parseDashboard(resp.data, this.userInfo)
+                            this.dashboardState = "Ready"
+                            //Set the page title
+                            document.title = "Recordm[" + this.dashboardName + "]"
+                        }
+                        catch(e) {
+                            this.error = "Error: error parsing dashboard " + newInstanceId + " (" + e + ")"
+                            this.dashboardState = "Error"
+                            console.error(e)
+                        }
+                    })
+                    .catch( (e) => {
+                        if( e.response && e.response.status && e.response.status === 403) {
+                            this.error = "New authorization required..."
+                        } else {
+                            this.error = "Error: error getting dashboard " + newInstanceId
+                        }
+                        this.dashboardState = "Error"
+                        console.error(e)
+                    })
                 }
-            }
-        },
-        methods: {
-            getDashboardQuery(name, userInfo) {
-                this.userInfo = userInfo
-                this.name = name
-
-                // TODO this is wrong we should only set the dashboard name after we pull it from recordm.
-                // there is no guarantee that dashboard exists
-                document.title = "Recordm[" + name + "]"
-                let groups = userInfo.groups.map(g=> "\"" + g.name + "\"").join(" OR ")
-
-                let nameQuery = "name.raw:\"" + name + "\" "
-                let accessQuery = " (groupaccess.raw:(" + groups + ") OR (-groupaccess:*) )"
-                return "(" + nameQuery + accessQuery +") OR id:" + name
             }
         }
     };

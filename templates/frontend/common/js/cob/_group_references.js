28df42fc01925cbd7b5eead7ca3a571aeb0d1137
Miguel Mesquita <mm@cob.pt>
2022-11-23 19:07:58 +0000

    feat(customize.common): improve _group_references count

--- templates/frontend/common/js/cob/_group_references.js
+++ templates/frontend/common/js/cob/_group_references.js
@@ -2,18 +2,21 @@
 // Use $style[use-reference-count] on the containing $group that you want to have the result
 cob.custom.customize.push(function (core, utils, ui) {
     core.customizeAllInstances(function(instance, presenter) {
-        var mo = new MutationObserver(function (mutations) {
-            mutations.forEach( m => {
-                let group = $(m.target).parentsUntil(".field-group.custom-use-reference-count").parent().find(".group-name")
-                let labelHTML = $(m.target).parent()[0].innerHTML
-                group[0].innerHTML = labelHTML.substring(0,labelHTML.indexOf("<div"))
-                $(m.target).parent().hide()
-            })
-        });
+        const update = (div) => {
+            let group = $(div).parentsUntil(".field-group.custom-use-reference-count").parent().find(".group-name")
+            let groupName = (group[0].children.length ? group[0].children[0].innerHTML : group[0].innerHTML).trim()
+            let labelHTML = $(div).parent()[0].innerHTML
+            let labelWithCount = labelHTML.substring(0,labelHTML.indexOf("<div"))
+            //Apenas muda o texto se o nome do $group for o mesmo que o do campo $references
+            //isto permite escolher qual o references a mostrar 
+            if(labelWithCount.indexOf(" <span>"+groupName+"</span>") == 0) {
+                group[0].innerHTML = labelWithCount
+                $(m.target).parent().parent().hide()
+            }
+        }
+        var mo = new MutationObserver( (mutations) => mutations.forEach( m => update(m.target)) );
 
-        let references = $('.field-group.custom-use-reference-count > ol > li > div > div.references-legend').toArray()
-        references.forEach( reference => {
-            mo.observe(reference, { childList: true, subtree: true });
-        }) 
+        let references = $('.field-group.custom-use-reference-count > ol > li:not(.hidden) > div > div.references-legend').toArray()
+        references.forEach( reference =>  mo.observe(reference.children[1], { childList: true }) );
     })
 })

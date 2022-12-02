f85f8a438574bfd36e97f651bce2d746ff69e112
Miguel Mesquita <mm@cob.pt>
2022-12-02 15:37:04 +0000

    refactor: replace by submodule

--- templates/dashboards/dash/src/utils/EsInstanceUtils.js
+++
@@ -1,8 +0,0 @@
-import {toEsFieldName} from "@cob/rest-api-wrapper/src/utils/ESHelper";
-
-function getValue(esInstance, fieldDefinition) {
-    let esFieldName = toEsFieldName(fieldDefinition.name);
-    return (esInstance[`${esFieldName}_reference`] || esInstance[esFieldName])
-}
-
-export {getValue}
